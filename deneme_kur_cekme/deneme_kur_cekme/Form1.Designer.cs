0c7d8a8a53fcab570b3d8f9d7c234bc5a839e0bf
Aziz07zer <aziz07ozer@gmail.com>
2024-10-11 10:09:04 +0300

    İnternetten kur çekme form uygulamasından

---
+++ deneme_kur_cekme/deneme_kur_cekme/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace deneme_kur_cekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbldolaralıs = new System.Windows.Forms.Label();
+            this.lbldolarsatıs = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbleurosatıs = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbleuroalıs = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dolar Alış:";
+            // 
+            // lbldolaralıs
+            // 
+            this.lbldolaralıs.AutoSize = true;
+            this.lbldolaralıs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldolaralıs.Location = new System.Drawing.Point(183, 16);
+            this.lbldolaralıs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbldolaralıs.Name = "lbldolaralıs";
+            this.lbldolaralıs.Size = new System.Drawing.Size(47, 25);
+            this.lbldolaralıs.TabIndex = 1;
+            this.lbldolaralıs.Text = "fiyat";
+            // 
+            // lbldolarsatıs
+            // 
+            this.lbldolarsatıs.AutoSize = true;
+            this.lbldolarsatıs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldolarsatıs.Location = new System.Drawing.Point(183, 68);
+            this.lbldolarsatıs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbldolarsatıs.Name = "lbldolarsatıs";
+            this.lbldolarsatıs.Size = new System.Drawing.Size(47, 25);
+            this.lbldolarsatıs.TabIndex = 3;
+            this.lbldolarsatıs.Text = "fiyat";
+            this.lbldolarsatıs.Click += new System.EventHandler(this.lbldolarsatıs_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dolar Satış:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 247);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 279);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(15, 326);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 39);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "TUTAR";
+            // 
+            // lbleurosatıs
+            // 
+            this.lbleurosatıs.AutoSize = true;
+            this.lbleurosatıs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbleurosatıs.Location = new System.Drawing.Point(183, 161);
+            this.lbleurosatıs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbleurosatıs.Name = "lbleurosatıs";
+            this.lbleurosatıs.Size = new System.Drawing.Size(47, 25);
+            this.lbleurosatıs.TabIndex = 10;
+            this.lbleurosatıs.Text = "fiyat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(16, 156);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "EURO Satış:";
+            // 
+            // lbleuroalıs
+            // 
+            this.lbleuroalıs.AutoSize = true;
+            this.lbleuroalıs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbleuroalıs.Location = new System.Drawing.Point(183, 111);
+            this.lbleuroalıs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbleuroalıs.Name = "lbleuroalıs";
+            this.lbleuroalıs.Size = new System.Drawing.Size(47, 25);
+            this.lbleuroalıs.TabIndex = 8;
+            this.lbleuroalıs.Text = "fiyat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(16, 106);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 31);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "EURO Alış:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dolar Alış",
+            "Dolar Satış",
+            "EURO Alış",
+            "EURO Satış"});
+            this.comboBox1.Location = new System.Drawing.Point(119, 206);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 24);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 210);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Kur\'u Seçin:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 375);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbleurosatıs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbleuroalıs);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbldolarsatıs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbldolaralıs);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbldolaralıs;
+        private System.Windows.Forms.Label lbldolarsatıs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbleurosatıs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbleuroalıs;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ deneme_kur_cekme/deneme_kur_cekme/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace deneme_kur_cekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldosya = new XmlDocument();
+            xmldosya.Load(bugun);
+            //dolar alış fiyatı.........
+            string dolaralıs = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerText;
+            lbldolaralıs.Text = dolaralıs;
+            //dolar5 satış fiyatı.......
+            string dolarsatıs = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            lbldolarsatıs.Text = dolarsatıs;
+
+            //euro alış fiyatı.........
+            string euroalıs = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerText;
+            lbleuroalıs.Text = euroalıs;
+            //euro satış fiyatı.......
+            string eurosatıs = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            lbleurosatıs.Text = eurosatıs;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedValue = comboBox1.SelectedItem.ToString();
+
+            double textBoxValue;
+            if (double.TryParse(textBox1.Text, out textBoxValue))
+            {
+                
+                if (textBoxValue == 0)
+                {
+                    MessageBox.Show("Bölme işlemi için 0'dan farklı bir değer girin.");
+                    return;
+                }
+
+                double result = 0;
+
+                // ComboBox'taki veriyi kontrol et
+                if (selectedValue == "Dolar Alış")
+                {
+                    double label1Value = double.Parse(lbldolaralıs.Text);
+                    result = textBoxValue/ (label1Value);
+                 
+                    label2.Text = "Sonuç: " + result.ToString()+" $";
+                }
+                else if (selectedValue == "Dolar Satış")
+                {
+                    double label3Value = double.Parse(lbldolarsatıs.Text);
+                    result = textBoxValue*(label3Value);
+                    label2.Text = "Sonuç: " + result.ToString()+" TL";
+                }
+                else if (selectedValue == "EURO Alış")
+                {
+                    double label5Value = double.Parse(lbleuroalıs.Text);
+                    result = textBoxValue/(label5Value);
+                    label2.Text = "Sonuç: " + result.ToString()+ " €";
+                }
+                else if (selectedValue == "EURO Satış")
+                {
+                    double label4Value = double.Parse(lbleurosatıs.Text);
+                    result =textBoxValue*(label4Value);
+                    label2.Text = "Sonuç: " + result.ToString()+ " TL";
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz seçim.");
+                    return;
+                }
+
+           
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir sayı girin.");
+            }
+        }
+
+        private void lbldolarsatıs_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
